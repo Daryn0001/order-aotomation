c084ea14f947a001a7fa00c305d41a9958ccfdd8
Daryn <daryn200150@gmail.com>
2023-03-23 00:58:05 +0600

    set web configuration

--- src/main/java/kz/sdu/stu/dsalimov/launcher/WebConfigDebug.java
+++ src/main/java/kz/sdu/stu/dsalimov/launcher/WebConfigDebug.java
@@ -1,19 +1,37 @@
 package kz.sdu.stu.dsalimov.launcher;
 
-import kz.sdu.stu.dsalimov.util.AbstractWebConfig;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-public class WebConfigDebug extends AbstractWebConfig {
+@Configuration
+public class WebConfigDebug {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .exposedHeaders("Content-Disposition")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://order-automation-frontend-lake.vercel.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Content-Disposition")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
     }
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders("*")
+//                .allowedMethods("*")
+//                .exposedHeaders("Content-Disposition")
+//                .allowCredentials(true);
+//    }
 }
