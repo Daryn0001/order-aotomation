0016e2719a7710a422928b5524e3d6c9733ca3ac
Daryn <daryn200150@gmail.com>
2023-06-04 18:43:43 +0600

    refactor

--- src/main/java/kz/sdu/stu/dsalimov/dao/BranchDao.java
+++ src/main/java/kz/sdu/stu/dsalimov/dao/BranchDao.java
@@ -10,19 +10,19 @@ import java.util.List;
 @Repository
 public interface BranchDao {
     @Select(//language=PostgreSQL
-            "SELECT * FROM branches")
+            "SELECT uuid, name, address, admin_id as adminId, company_uuid as companyUuid FROM branches")
     List<Branch> findAll();
 
     @Select(//language=PostgreSQL
-            "SELECT * FROM branches WHERE uuid = #{uuid}")
+            "SELECT  uuid, name, address, admin_id as adminId, company_uuid as companyUuid FROM branches WHERE uuid = #{uuid}")
     Branch findByUuid(String uuid);
 
     @Insert(//language=PostgreSQL
-            "INSERT INTO branches (uuid, name, address, admin_uuid, company_id) VALUES (#{uuid}, #{name}, #{address}, #{adminUuid}, #{companyId})")
+            "INSERT INTO branches (uuid, name, address, admin_id, company_uuid) VALUES (#{uuid}, #{name}, #{address}, #{adminUuid}, #{companyId})")
     void insert(Branch branch);
 
     @Update(//language=PostgreSQL
-            "UPDATE branches SET name = #{name}, address = #{address}, admin_uuid = #{adminUuid}, company_id = #{companyId} WHERE uuid = #{uuid}")
+            "UPDATE branches SET name = #{name}, address = #{address}, admin_id = #{adminUuid}, company_uuid = #{companyId} WHERE uuid = #{uuid}")
     void update(Branch branch);
 
     @Delete(//language=PostgreSQL
@@ -38,11 +38,11 @@ public interface BranchDao {
     void updateAddress(@Param("uuid") String uuid, @Param("address") String address);
 
     @Update(//language=PostgreSQL
-            "UPDATE branches SET admin_uuid = #{adminUuid} WHERE uuid = #{uuid}")
+            "UPDATE branches SET admin_id = #{adminUuid} WHERE uuid = #{uuid}")
     void updateAdminUuid(@Param("uuid") String uuid, @Param("adminUuid") Long adminUuid);
 
     @Update(//language=PostgreSQL
-            "UPDATE branches SET company_id = #{companyId} WHERE uuid = #{uuid}")
+            "UPDATE branches SET company_uuid = #{companyId} WHERE uuid = #{uuid}")
     void updateCompanyId(@Param("uuid") String uuid, @Param("companyId") String companyId);
 
 }
